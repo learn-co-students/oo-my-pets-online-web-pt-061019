b9f9bd9730bc4e44479274fe7c15e3e029c5ae12
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-05 22:03:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Dog
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+    @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @@all << self
+    @owner = owner
+  end
+
+  def self.all
+    @@all
+  end
+
 end
