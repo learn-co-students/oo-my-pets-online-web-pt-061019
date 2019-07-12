c83ec50dadad7ecce3dae364704177fbe17f693c
David Foote <david.a.foote@gmail.com>
2019-07-12 23:45:50 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name, :owner
+  
+  def initialize(name)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  @@count = 0
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@count += 1
+  end
+  
+  def species
+    @species = "human"
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@count
+  end
 end
