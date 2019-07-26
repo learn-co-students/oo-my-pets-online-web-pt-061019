954f56114010391b172b90c133cae772a2d438ca
Valena <valenadelvalle4@gmail.com>
2019-07-26 17:42:21 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @species = species
+    @name = name
+    @@all << self 
+  end
+
+  def species
+    @species = "human"
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.count 
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
