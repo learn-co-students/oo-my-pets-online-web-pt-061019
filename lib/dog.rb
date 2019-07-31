d6697cf9f820283ebf3ea93aaeda1788c006be05
Rene Cocom <fasnimo@gmail.com>
2019-07-31 06:30:57 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+      @owner = owner
+      @name = name
+      @owner.dogs<<self
+      @mood = "nervous"
+      @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # def self.count
+  #   @@all.length
+  # end
+
 end
